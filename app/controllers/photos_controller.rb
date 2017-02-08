f5879a27bcfc06def981b9053137acc29fb110db
K-Wei <kevinwei@users.noreply.github.com>
2017-02-07 22:39:43 -0600

    finished
    100%, can create, read, update, delete.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,46 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
 
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    # render("create_row.html.erb")
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    id = params[:id]
+    p = Photo.find(id)
+    p.destroy
+
+    # render("destroy.html.erb")
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+
+    render("edit_form.html.erb")
+  end
+
+  def update_photo
+    p = Photo.find_by({ :id => params[:id]})
+    p.caption = params[:new_caption]
+    p.source = params[:new_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos/#{p.id}")
+  end
 end
